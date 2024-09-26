6fa4c854c253e1dafa85fc27221e18157150e5d5
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 16:58:43 +0600

    updating the number of question

--- script.js
+++ script.js
@@ -17,26 +17,39 @@ continueBtn.onclick = ()=>{
     infoBox.classList.remove('activeInfo');
     quizBox.classList.add('activeQuiz');
     showQuestion(queNum);
+    queCounter(1)
 }
 
 let queNum = 0;
+let queCount = 1;
 
 let nextBtn = document.querySelector('.next-que');
 
 nextBtn.onclick = () =>{
-    queNum++;
-    showQuestion(queNum);
+    if (queNum < questions.length - 1){
+        queNum++;
+        queCount++;
+        showQuestion(queNum);
+        queCounter(queCount)
+    }else{
+        console.log('complete que')
+    }
 }
 
 function showQuestion(index){
     let queText = document.querySelector('.que-text');
     let optionList = document.querySelector('.options-list');
-    let queTag = ` <span>${questions[index].question}</span>`;
+    let queTag = ` <span>${questions[index].number}.${questions[index].question}</span>`;
     let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>`
                     + `<div class="option"><span>${questions[index].options[1]}</span></div>`
                     +`<div class="option"><span>${questions[index].options[2]}</span></div>`
                     + `<div class="option"><span>${questions[index].options[3]}</span></div>`;
     queText.innerHTML = queTag;
     optionList.innerHTML = optionTag;
+}
 
+function queCounter(index){
+    const bottomQueCounter = document.querySelector('.total-que');
+    let totalQueTag = ` <span><p>${index}</p> of <p>${questions.length}</p> Question </span>`;
+    bottomQueCounter.innerHTML = totalQueTag;
 }
