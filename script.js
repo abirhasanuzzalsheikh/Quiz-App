324527d8e5d24a78d1aae930d7c3ca3e43a6ad78
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 17:56:55 +0600

    add optionSelected function and compare user answer and correct answer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const infoBox = document.querySelector('.info-box');
 const continueBtn = infoBox.querySelector('.buttons .restart-btn');
 const exitBtn = document.querySelector('.info-box .quit-btn'); // Change made here
 const quizBox = document.querySelector('.quiz-box');
+let optionList = document.querySelector('.options-list');
 
 
 startBtn.onclick = () => {
@@ -38,7 +39,6 @@ nextBtn.onclick = () =>{
 
 function showQuestion(index){
     let queText = document.querySelector('.que-text');
-    let optionList = document.querySelector('.options-list');
     let queTag = ` <span>${questions[index].number}.${questions[index].question}</span>`;
     let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>`
                     + `<div class="option"><span>${questions[index].options[1]}</span></div>`
@@ -46,6 +46,40 @@ function showQuestion(index){
                     + `<div class="option"><span>${questions[index].options[3]}</span></div>`;
     queText.innerHTML = queTag;
     optionList.innerHTML = optionTag;
+    const options = document.querySelectorAll('.option');
+    options.forEach(option => {
+        option.addEventListener('click', () => optionSelected(option));
+    });
+}
+
+let tickIcon  = ` <div class="icon tick"><i class="fas fa-check"></i></div>`;
+let crossIcon = `<div class="icon cross"><i class="fas fa-times"></i></div>`;
+
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let rightAnswer = questions[queNum].answer;
+    let allOption = optionList.children.length;
+    if(userAnswer === rightAnswer){
+        answer.classList.add('correct');
+        answer.insertAdjacentHTML('beforeend',tickIcon);
+        console.log('correct')
+    }else{
+        answer.classList.add('incorrect');
+        answer.insertAdjacentHTML('beforeend',crossIcon);
+        console.log('incorrect')
+
+        for(let i = 0; i < allOption; i++){
+            if(optionList.children[i].textContent == rightAnswer){
+                optionList.children[i].setAttribute('class','option correct');
+                optionList.children[i].insertAdjacentHTML('beforeend',tickIcon);
+            }
+        }
+    }
+
+    for(let i = 0; i < allOption; i++){
+        optionList.children[i].classList.add('disabled');
+    }
 }
 
 function queCounter(index){
