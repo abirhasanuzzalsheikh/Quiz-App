0f4222097d2b6a63e9c3d8ef92ed50ff48b0bef3
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 09:33:25 +0600

    getting all the required els

---
+++ script.js
@@ -0,0 +1,4 @@
+const startBtn =  document.querySelector('.start-btn button');
+const infoBox = document.querySelector('.info-box');
+const exitBtn = document.querySelector('.buttons .quit-btn');
+const continueBtn = infoBox.querySelector('.buttons .restart-btn');
