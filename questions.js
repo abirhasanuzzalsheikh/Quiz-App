fe6f6c03011a15505ded2110383b0d599dbb5a95
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 11:30:04 +0600

    add question.js file and make some list of question and answer in array

---
+++ questions.js
@@ -0,0 +1,58 @@
+export let questions = [
+    {
+        number: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        number: 2,
+        question: "Which HTML tag is used to define an internal style sheet?",
+        answer: "<style>",
+        options: [
+            "<css>",
+            "<script>",
+            "<style>",
+            "<styles>"
+        ]
+    },
+    {
+        number: 3,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Creative Style System",
+            "Cascading Style Sheets",
+            "Computer Style Sheets",
+            "Colorful Style Sheets"
+        ]
+    },
+    {
+        number: 4,
+        question: "Which HTML attribute is used to define the inline styles?",
+        answer: "style",
+        options: [
+            "class",
+            "styles",
+            "style",
+            "font"
+        ]
+    },
+    {
+        number: 5,
+        question: "What is the correct HTML element for inserting a line break?",
+        answer: "<br>",
+        options: [
+            "<break>",
+            "<lb>",
+            "<breakline>",
+            "<br>"
+        ]
+    },
+    
+];
