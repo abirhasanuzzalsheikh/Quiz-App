7fef3cf6ff2df04188462bb2fe59387dc4038202
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 09:36:52 +0600

    make start button interactive

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const startBtn =  document.querySelector('.start-btn button');
 const infoBox = document.querySelector('.info-box');
 const exitBtn = document.querySelector('.buttons .quit-btn');
 const continueBtn = infoBox.querySelector('.buttons .restart-btn');
+
+
+startBtn.onclick = ()=>{
+    infoBox.classList.add('activeInfo');
+}
