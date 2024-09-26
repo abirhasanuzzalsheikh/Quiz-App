43ec16920e30a134691b7ec48823fbe2135623f9
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 11:50:28 +0600

    showing questions and option in quiz box using js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { questions } from "./questions.js";
+
 const startBtn = document.querySelector('.start-btn button');
 const infoBox = document.querySelector('.info-box');
 const continueBtn = infoBox.querySelector('.buttons .restart-btn');
@@ -14,4 +16,20 @@ exitBtn.onclick = ()=>{
 continueBtn.onclick = ()=>{
     infoBox.classList.remove('activeInfo');
     quizBox.classList.add('activeQuiz');
+    showQuestion(2);
+}
+
+let queNum = 0;
+
+function showQuestion(index){
+    let queText = document.querySelector('.que-text');
+    let optionList = document.querySelector('.options-list');
+    let queTag = ` <span>${questions[index].question}</span>`;
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>`
+                    + `<div class="option"><span>${questions[index].options[1]}</span></div>`
+                    +`<div class="option"><span>${questions[index].options[2]}</span></div>`
+                    + `<div class="option"><span>${questions[index].options[3]}</span></div>`;
+    queText.innerHTML = queTag;
+    optionList.innerHTML = optionTag;
+
 }
