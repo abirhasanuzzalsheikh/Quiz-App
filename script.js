db61b6b6607a56a5674272399acff4346380ab28
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 11:03:54 +0600

    make the quit button interactive

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const startBtn =  document.querySelector('.start-btn button');
+const startBtn = document.querySelector('.start-btn button');
 const infoBox = document.querySelector('.info-box');
-const exitBtn = document.querySelector('.buttons .quit-btn');
 const continueBtn = infoBox.querySelector('.buttons .restart-btn');
+const exitBtn = document.querySelector('.info-box .quit-btn'); // Change made here
 
-
-startBtn.onclick = ()=>{
+startBtn.onclick = () => {
     infoBox.classList.add('activeInfo');
+} 
+exitBtn.onclick = ()=>{
+    infoBox.classList.remove('activeInfo');
 }
