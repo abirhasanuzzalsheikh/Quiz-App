a5dc221070653823c17255319c227d395abde376
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 11:13:13 +0600

    make the continue button interactive

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const startBtn = document.querySelector('.start-btn button');
 const infoBox = document.querySelector('.info-box');
 const continueBtn = infoBox.querySelector('.buttons .restart-btn');
 const exitBtn = document.querySelector('.info-box .quit-btn'); // Change made here
+const quizBox = document.querySelector('.quiz-box');
+
 
 startBtn.onclick = () => {
     infoBox.classList.add('activeInfo');
 } 
 exitBtn.onclick = ()=>{
     infoBox.classList.remove('activeInfo');
+}
+continueBtn.onclick = ()=>{
+    infoBox.classList.remove('activeInfo');
+    quizBox.classList.add('activeQuiz');
 }
