3f6b59c24fb4d3066615073ad978e1216b371035
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-09-26 19:33:43 +0600

    update the timer,time line and next que btn

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const continueBtn = infoBox.querySelector('.buttons .restart-btn');
 const exitBtn = document.querySelector('.info-box .quit-btn'); // Change made here
 const quizBox = document.querySelector('.quiz-box');
 let optionList = document.querySelector('.options-list');
+const timeCount = document.querySelector('.timer-sec');
+const timeLine = document.querySelector('.time-line');
 
 
 startBtn.onclick = () => {
@@ -18,11 +20,17 @@ continueBtn.onclick = ()=>{
     infoBox.classList.remove('activeInfo');
     quizBox.classList.add('activeQuiz');
     showQuestion(queNum);
-    queCounter(1)
+    queCounter(1);
+    startTimer(15);
+    startTimerLine(0);
 }
 
 let queNum = 0;
 let queCount = 1;
+let counter;
+let timeValue = 15;
+let widthValue = 0;
+let counterLine;
 
 let nextBtn = document.querySelector('.next-que');
 
@@ -32,6 +40,11 @@ nextBtn.onclick = () =>{
         queCount++;
         showQuestion(queNum);
         queCounter(queCount)
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        nextBtn.style.display = 'none';
     }else{
         console.log('complete que')
     }
@@ -57,6 +70,8 @@ let crossIcon = `<div class="icon cross"><i class="fas fa-times"></i></div>`;
 
 
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine)
     let userAnswer = answer.textContent;
     let rightAnswer = questions[queNum].answer;
     let allOption = optionList.children.length;
@@ -80,10 +95,38 @@ function optionSelected(answer){
     for(let i = 0; i < allOption; i++){
         optionList.children[i].classList.add('disabled');
     }
+    nextBtn.style.display = 'block';
 }
 
 function queCounter(index){
     const bottomQueCounter = document.querySelector('.total-que');
     let totalQueTag = ` <span><p>${index}</p> of <p>${questions.length}</p> Question </span>`;
     bottomQueCounter.innerHTML = totalQueTag;
+}
+
+function startTimer(time){
+    counter = setInterval(timer,1000);
+    function timer(){
+        timeCount.textContent =  time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = '0' + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter)
+            timeCount.textContent = '00';
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer,29);
+    function timer(){
+        time++;
+        timeLine.style.width = time + 'px';
+        if(time > 549){
+            clearInterval(counterLine);
+        }
+    }
 }
